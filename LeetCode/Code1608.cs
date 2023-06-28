2d7125b2f21656f11717e6e931503539372034de
Logarius <415753928@qq.com>
2023-06-28 20:29:58 +0800

    1

---
+++ LeetCode/Code1608.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class Code1608
+    {
+        public int SpecialArray(int[] nums)
+        {
+            // var dic = new Dictionary<int, int>();
+            // for (int i = 0; i < nums.Length; i++)
+            // {
+            //     for (int j = 1; j <= nums[i]; j++)
+            //     {
+            //         if (dic.ContainsKey(j))
+            //         {
+            //             dic[j] += 1;
+            //         }
+            //         else
+            //         {
+            //             dic[j] = 1;
+            //         }
+            //     }
+            // }
+            //
+            // foreach (var pair in dic)
+            // {
+            //     if (pair.Key == pair.Value)
+            //     {
+            //         return pair.Value;
+            //     }
+            // }
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var num = nums.Length - i;
+                var start = i == 0 ? 1 : nums[i - 1] + 1;
+                var end = nums[i];
+                for (int j = start; j <= end; j++)
+                {
+                    if (num == j)
+                    {
+                        return j;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -6,8 +6,8 @@ namespace LeetCode
     {
         public static void Main(string[] args)
         {
-            var code = new Code2485();
-            var res = code.PivotInteger(8);
+            var code = new Code1608();
+            var res = code.SpecialArray(new[] { 3,5 });
             Console.WriteLine(res);
         }
     }
