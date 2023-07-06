82c811f124a593a4b55a3a7aa87508d2a1b1e639
Logarius <415753928@qq.com>
2023-07-06 17:20:09 +0800

    1

---
+++ LeetCode/Code/Code14.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode
+{
+    public class Code14
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            var first = strs[0];
+            var index = 0;
+            while (index < first.Length)
+            {
+                var chr = first[index];
+                for (int i = 1; i < strs.Length; i++)
+                {
+                    if (index >= strs[i].Length || strs[i][index] != chr)
+                    {
+                        goto end;
+                    }
+                }
+
+                index++;
+            }
+
+            end:
+
+            if (index > 0)
+            {
+                return strs[0].Substring(0, index);
+            }
+
+            return "";
+        }
+    }
+}
