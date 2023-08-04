7f841b06262c4f5824bc32b3cfac525a4d2b27f0
Logarius <415753928@qq.com>
2023-08-04 11:51:19 +0800

    1

---
+++ LeetCode/Easy/Code100.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Code100
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null)
+                return q == null;
+            if (q == null)
+                return false;
+            return p.val == q.val && IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+}
