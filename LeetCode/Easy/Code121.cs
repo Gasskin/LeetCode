1a2da72f5d996527d94fc9d31a17169260212ba8
Logarius <415753928@qq.com>
2023-08-07 20:54:32 +0800

    1

---
+++ LeetCode/Easy/Code121.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class Code121
+    {
+        /// <summary>
+        /// 想要知道第i天的收益，只需要知道前i天的最低点
+        /// f(i)=前i天的最低点
+        /// f(i) = min[f(i-1),price(i)]
+        /// </summary>
+        /// <param name="prices"></param>
+        /// <returns></returns>
+        public int MaxProfit(int[] prices)
+        {
+            // var res = new int[prices.Length];
+            // var profit = 0;    
+            // for (int i = 0; i < prices.Length; i++)
+            // {
+            //     if (i == 0)
+            //     {
+            //         res[i] = prices[i];
+            //     }
+            //     else
+            //     {
+            //         res[i] = Math.Min(res[i - 1], prices[i]);
+            //     }
+            //
+            //     if (prices[i] - res[i] > profit)
+            //     {
+            //         profit = prices[i] - res[i];
+            //     }
+            // }
+            //
+            // return profit;
+
+            var profit = 0;
+            var low = prices[0];
+            foreach (var t in prices)
+            {
+                low = Math.Min(low, t);
+                profit = t - low > profit ? t - low : profit;
+            }
+
+            return profit;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -6,10 +6,9 @@ namespace LeetCode
     {
         public static void Main(string[] args)
         {
-            var code = new Code88_2();
-            // var res = code.Merge(new[] { 1, 2, 3, 0, 0, 0 }, 3, new[] { 2, 5, 6 }, 3);
-            // Console.WriteLine(res);
-            code.Merge(new[] { 1, 2, 3, 0, 0, 0 }, 3, new[] { 2, 5, 6 }, 3);
+            var code = new Code121();
+            var res = code.MaxProfit(new[] { 7, 1, 5, 3, 6, 4 });
+            Console.WriteLine(res);
         }
     }
 }
