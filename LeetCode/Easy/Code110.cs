f7268db9cf12c61cd99ed5025be2b0d8f496cf31
Logarius <415753928@qq.com>
2023-08-16 15:40:21 +0800

    1

---
+++ LeetCode/Easy/Code110.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class Code110
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public bool IsBalanced(TreeNode root)
+        {
+            if (root == null)
+                return true;
+            var l = GetDepth(root.left);
+            var r = GetDepth(root.right);
+            return Math.Abs(l - r) <= 1 && IsBalanced(root.left) && IsBalanced(root.right);
+        }
+
+        private int GetDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            var l = GetDepth(root.left);
+            var r = GetDepth(root.right);
+            return Math.Max(l, r) + 1;
+        }
+    }
+}
