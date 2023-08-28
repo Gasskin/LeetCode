e0f0419bd6a19d079537ee4512f56bbf45c4b3f5
Logarius <415753928@qq.com>
2023-08-28 20:08:50 +0800

    1

---
+++ LeetCode/Easy/Code112.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode
+{
+    public class Code112
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if (root == null)
+                return false;
+            if (root.left == null && root.right == null)
+                return root.val == targetSum;
+            return HasPathSum(root.left, targetSum - root.val) || HasPathSum(root.right, targetSum - root.val);
+        }
+    }
+}
